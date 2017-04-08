8df289bc3396462e1a9846e4d915f4a26c28ede6
xiangruhuang <xrhuang@cs.utexas.edu>
2017-04-08 18:18:50 -0500

    added summary

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ import tensorflow as tf
 flags = tf.flags
 """     System and File I/O         """
 flags.DEFINE_string("data_path", 'data/merged_100.npy', "path to train/test/valid data.")
-flags.DEFINE_string("model_dir", None, "where to output model and tensorboard")
+flags.DEFINE_string("model_dir", '.', "where to output model and tensorboard")
 flags.DEFINE_string("device", "1", "visible device")
 
 """     Data Specs                  """
 flags.DEFINE_integer("height", 240, "height of images")
 flags.DEFINE_integer("width", 320, "width of images")
 flags.DEFINE_integer("channels", 4, "number of channels")
+flags.DEFINE_float("mask_rate", 0.1, "fraction of depth data that can be seen")
 
 """     Network Architecture        """
 flags.DEFINE_string("kernel_size", '7,7', "size of kernel {7,7}")
@@ -62,17 +63,16 @@ def hourglass(inputs, depth):
 
     return last_layer 
 
-def build_dataflow(data):
+def build_dataflow(data, indices):
     #indices = tf.placeholder(shape=[FLAGS.batch_size], dtype=tf.int32)
     sample_size = data.shape.as_list()[0]
     print('total_sample_size=%d' % sample_size)
     #shuffle = tf.range(0, limit=sample_size)
-    shuffle = tf.random_shuffle(tf.range(0, limit=sample_size))
-    indices = shuffle[:FLAGS.batch_size]
     batch_data = tf.gather(data, indices)
     mask = tf.random_uniform(shape=[FLAGS.batch_size, FLAGS.height, FLAGS.width],
-            minval=0, maxval=2, dtype=tf.int32)
-    mask = tf.to_float(mask)
+            minval=0, maxval=1, dtype=tf.float32)
+    mask = tf.sigmoid(mask - tf.constant(FLAGS.mask_rate))
+    mask = tf.round(mask)
     channels = tf.unstack(batch_data, axis=3)
     grouth_truth = channels[3]
     channels[3] = tf.multiply(channels[3], mask)
@@ -95,6 +95,12 @@ def build_dataflow(data):
             padding='same',
             )
 
+    compare_images = tf.concat([last_layer, tf.expand_dims(ground_truth)],
+        axis=1)
+    rgb = tf.image.grayscale_to_rgb(compare_images)
+    rgb_summary = tf.summary.image('image', rgb)
+    rgb_summary = tf.summary.merge([rgb_summary])
+    
     #print(last_layer.shape)
     loss = tf.nn.l2_loss(tf.squeeze(last_layer, axis=-1)-grouth_truth)
 
@@ -107,18 +113,31 @@ def build_dataflow(data):
     #        learning_rate=FLAGS.learning_rate,
     #        optimizer="SGD"
     #        )
-    evals = {'opt':opt, 'loss':loss}
-    return evals
+    train_evals = {'opt':opt, 'loss':loss}
+    summary_evals = {'summary':rgb_summary}
+    return train_evals, summary_evals
 
 def main(_):
+    print('batch_size=%d' % FLAGS.batch_size)
     data = numpy.load(FLAGS.data_path)
     data = tf.convert_to_tensor(data, dtype=tf.float32)
-    evals = build_dataflow(data)
+    shuffle = tf.random_shuffle(tf.range(0, limit=sample_size)) 
+    shuffle_indices = shuffle[:FLAGS.batch_size]
+    fixed_indices = tf.range(0, FLAGS.batch_size)
+    indices = tf.placeholder(shape=[FLAGS.batch_size], dtype=tf.int32)
+
+    summary_writer = tf.summary.FileWriter(FLAGS.model_dir+'/tensorboard/')
+    train_evals, summary_evals = build_dataflow(data, indices)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for i in range(1000):
-            vals = sess.run(evals)
+        for i in range(10000):
+            vals = sess.run(train_evals,
+                feed_dict={indices:sess.run(shuffle_indices)})
             print('iter=%d, loss=%f' % (i, vals['loss']))
+            if i % 30 == 0:
+                vals = sess.run(summary_evals,
+                    feed_dict={indices:sess.run(fixed_indices)})
+                summary_writer.add_summary(vals['summary'], i)
 
 if __name__ == "__main__":
     tf.app.run()
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all: data/merged_100.npy
-	((stdbuf -oL python main.py) 2>&1) >> mylog
+	((stdbuf -oL python main.py --batch_size=15) 2>&1) >> mylog
 
 data/merged_100.npy:
 	make -C ./data merged_100.npy
